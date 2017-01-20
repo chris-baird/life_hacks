952b70bddabb00e7b698251733a43fe673be2f18
chris baird <cab3953@gmail.com>
2017-01-19 16:02:40 -0800

    fixed some stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
     end
 
     def authorize
-      redirect_to login_path, alert: 'Not authorized - you must be logged in!' if current_user.nil?
+      redirect_to login_path, alert: 'You must be logged in to do that.' if current_user.nil?
     end
 end
